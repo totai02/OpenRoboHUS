bb0bb038f52b6ef0752d1dcacdf43d4365d5e756
TaiTT <totai9702@gmail.com>
2019-01-13 05:16:46 +0000

    Hello world Bai2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    string name, address;
+    int age;
+
+    cout << "Nhập tên của bạn: ";
+    getline (cin, name);
+
+    cout << "Nhập tuổi: ";
+    cin >> age;
+
+    cout << "Địa chỉ: ";
+    cin.ignore();           // câu lệnh dùng để xóa buffer, nếu ko có lệnh này, address sẽ nhận dấu "\n"
+                            // của lúc nhập tuổi để lại.
+    getline (cin, address);
+
+    cout << "Tên: " << name << ", Tuổi: " << age << ", Địa chỉ: " << address << endl;
+
     return 0;
 }
