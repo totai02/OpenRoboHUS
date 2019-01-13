714b45865d1ff3973c42435b5b66fc0375f5fab9
TaiTT <totai9702@gmail.com>
2019-01-13 08:11:40 +0000

    DC Servo: Bai3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <signal.h>
 #include <pigpio.h>
+#include <wiringPi.h>
 #include <cmath>
 
 #define SERVO1 19
+#define BTN1 7
+#define BTN2 0
+#define BTN3 21
+#define BTN4 22
 
 using namespace std;
 
@@ -19,25 +24,45 @@ void setServo(int servo, float angle)
     gpioServo(servo, gpioValue);
 }
 
+void initPin()
+{
+    pinMode(BTN1, INPUT);
+    pinMode(BTN2, INPUT);
+    pinMode(BTN3, INPUT);
+    pinMode(BTN4, INPUT);
+
+    pullUpDnControl(BTN1, PUD_UP);
+    pullUpDnControl(BTN2, PUD_UP);
+    pullUpDnControl(BTN3, PUD_UP);
+    pullUpDnControl(BTN4, PUD_UP);
+}
+
 int main(int argc, char *argv[])
 {
     // PiGpio setup
     if (gpioInitialise() < 0) return -1;
 
-    setServo(SERVO1, 0);
+    // wiringPi setup
+    wiringPiSetup () ;
 
-    int servoDir = 1;
-    int angle = 0;
+    setServo(SERVO1, 10);
+
     // loop
     while (true)
     {
-        if (angle + servoDir > 180 || angle + servoDir < 0)
+        if (digitalRead(BTN1) == LOW)
         {
-            servoDir = -servoDir;
+            for (int angle = 10; angle <= 180; angle+=10)
+            {
+                setServo(SERVO1, angle);
+                time_sleep(0.01);
+            }
+            for (int angle = 180; angle >= 10; angle-=10)
+            {
+                setServo(SERVO1, angle);
+                time_sleep(0.01);
+            }
         }
-        angle = angle + servoDir;
-        setServo(SERVO1, angle);
-        time_sleep(0.01);
     }
 
     return 0;
