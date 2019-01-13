33029d9436629dc8cb6a7e377595ab786fd88f71
TaiTT <totai9702@gmail.com>
2019-01-13 09:11:59 +0000

    DC Motor: Bai3

--- main.cpp
+++ main.cpp
@@ -2,14 +2,64 @@
 #include <softPwm.h>
 #include <wiringPi.h>
 
-#define PWM1 16
-#define PWM2 6
+#define PWM1_1 16
+#define PWM1_2 6
+#define PWM2_1 31
+#define PWM2_2 11
+
 
 using namespace std;
 
 void initPin()
-{    softPwmCreate(PWM1, 0, 255);
+{
+    softPwmCreate(PWM1, 0, 255);
     softPwmCreate(PWM2, 0, 255);
+}
+
+void forward()
+{
+    softPwmWrite(PWM1_1, 150);
+    softPwmWrite(PWM1_2, 0);
+    softPwmWrite(PWM2_1, 150);
+    softPwmWrite(PWM2_2, 0);
+    delay(1000);
+    stop();
+}
+
+void backward()
+{
+    softPwmWrite(PWM1_1, 0);
+    softPwmWrite(PWM1_2, 150);
+    softPwmWrite(PWM2_1, 0);
+    softPwmWrite(PWM2_2, 150);
+    delay(1000);
+    stop();
+}
+
+void turnLeft()
+{
+    softPwmWrite(PWM1_1, 150);
+    softPwmWrite(PWM1_2, 0);
+    softPwmWrite(PWM2_1, 0);
+    softPwmWrite(PWM2_2, 150);
+    delay(500);
+    stop();
+}
+
+void turnRight()
+{
+    softPwmWrite(PWM1_1, 0);
+    softPwmWrite(PWM1_2, 150);
+    softPwmWrite(PWM2_1, 150);
+    softPwmWrite(PWM2_2, 0);
+    delay(500);
+    stop();
+}
+
+void stop()
+{
+    softPwmWrite(PWM1, 0);
+    softPwmWrite(PWM2, 0);
 }
 
 int main(int argc, char *argv[])
@@ -19,23 +69,33 @@ int main(int argc, char *argv[])
 
     initPin();
 
-    int speed = 255;
+    int dir;
 
     // loop
     while (true)
     {
-        cin >> speed;
+        cin >> dir;
 
-        if (speed > 0)
+        if (dir == 1)
         {
-            softPwmWrite(PWM1, speed);
-            softPwmWrite(PWM2, 0);
+            forward();
         }
-        else
+
+        if (dir == 2)
         {
-            softPwmWrite(PWM1, 0);
-            softPwmWrite(PWM2, -speed);
+            backward();
         }
+
+        if (dir == 3)
+        {
+            turnLeft();
+        }
+
+        if (dir == 4)
+        {
+            turnRight();
+        }
+
     }
 
     return 0;
