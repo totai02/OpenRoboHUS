1f3efa143cdd34ad5cef0a12c5df04f8adf74aeb
TaiTT <totai9702@gmail.com>
2019-01-13 09:59:00 +0000

    Acquisition Image: Bai1

--- main.cpp
+++ main.cpp
@@ -1,53 +1,40 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <wiringPi.h>
+
+#define BTN1 7
 
 using namespace std;
 using namespace cv;
 
-int lowH = 0;
-int highH = 255;
-int lowS = 0;
-int highS = 255;
-int lowV = 0;
-int highV = 255;
-
-Mat binaryImage(const Mat &src)
+void initPin()
 {
-    Mat imgHSV, binary;
-    cvtColor(src, imgHSV, COLOR_BGR2HSV);
+    pinMode(BTN1, INPUT);
 
-    inRange(imgHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), binary);
-
-    return binary;
+    pullUpDnControl(BTN1, PUD_UP);
 }
 
 int main(int argc, char *argv[])
 {
+    // wiringPi setup
+    wiringPiSetup () ;
+
+    initPin();
+
     namedWindow( "Video", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Threshold", CV_WINDOW_AUTOSIZE );
-
-    cvCreateTrackbar("LowH", "Threshold", &lowH, 179);
-    cvCreateTrackbar("HighH", "Threshold", &highH, 179);
-
-    cvCreateTrackbar("LowS", "Threshold", &lowS, 255);
-    cvCreateTrackbar("HighS", "Threshold", &highS, 255);
-
-    cvCreateTrackbar("LowV", "Threshold", &lowV, 255);
-    cvCreateTrackbar("HighV", "Threshold", &highV, 255);
 
     VideoCapture capture = VideoCapture(0);
     Mat src, binary;
 
     while (waitKey(1) != 'q')
     {
-        capture >> src;
-        resize(src, src, src.size() / 3);
-
-        binary = binaryImage(src);
-
-        imshow("Video", src);
-        imshow("Threshold", binary);
+        if (digitalRead(BTN1) == LOW)
+        {
+            capture >> src;
+            imshow("Video", src);
+            delay(250);
+        }
     }
     return 0;
 }
